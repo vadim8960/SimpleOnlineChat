6258915795081dfb0580e447038186feab2e5401
vadim8960 <akimovvadim779@gmail.com>
2024-02-22 00:46:05 +0300

    Initial commit

---
+++ src/App.hpp
@@ -0,0 +1,12 @@
+//
+// Created by vadim on 22.02.24.
+//
+
+#ifndef SIMPLEONLINECHAT_APP_HPP
+#define SIMPLEONLINECHAT_APP_HPP
+
+class App {
+
+};
+
+#endif //SIMPLEONLINECHAT_APP_HPP
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by vadim on 22.02.24.
+//
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
